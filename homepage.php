012aa3005112bb40e8352cc1e54b0ebf1f4a8ea0
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-11 10:15:09 +0100

    GGGGGOOOOOOOOOOOOOOO!!!!!!!

--- homepage.php
+++ homepage.php
@@ -1,6 +1,26 @@
 <html>
 <head>
     <title>HOMEPAGE</title>
+
+    <?PHP
+
+    session start();
+    $access_level = $_COOKIE['access_level_cookie'];
+
+    displayAccessLevelInformation($access_level);
+
+    function displayAccessLevelInformation($access_level) {
+    if ($access_level == "standarduser") {
+    echo "<p>You are currently logged in as a standard user</p>";
+    }
+    elseif ($access_level == "root") {
+    echo "<p>You are currently logged in as a root user</p>";
+    echo "<p>You now have access to additional administrative features</p>";
+    }
+    }
+
+?>
+
 </head>
 <header>
 
