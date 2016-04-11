d2cacbc26e7b74a55ddcf746664ee3c548f177bf
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-11 10:38:07 +0100

    GGGGGOOOOOOOOOOOOOOO!!!!!!!

--- checklogin.php
+++ checklogin.php
@@ -1,20 +1,24 @@
 <html>
 <head>
 <title>CHECK LOGIN PAGE</title>
-
-    <?php
-    $username = $_POST['user'];
-
-    ?>
 </head>
-<form action="homepage.php" method="post">
-    <label for="user">Username: </label>
-    <input type="text" id="user" name="user" value=$username readonly />
-</form>
 
 <?php
 $username = $_POST['user'];
 $password = $_POST['password'];
+
+$newhtml =
+    <<<NEWHTML
+
+
+
+<form action="homepage.php" method="post">
+    <label for="user">Username: </label>
+    <input type="text" id="user" name="user" value = $username readonly />
+</form>
+
+NEWHTML;
+
 
 if($username == "Mike" && $password == '123456') {
     setcookie('user_cookie', $username);
