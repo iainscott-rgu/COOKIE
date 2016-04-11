5bf6f6083758a166f7f1cc241614765b5cb0a999
iainscott-rgu <odyssey3007@hotmail.com>
2016-04-11 10:03:01 +0100

    GGGGGOOOOOOOOOOOOOOO!!!!!!!

--- checklogin.php
+++ checklogin.php
@@ -1,6 +1,17 @@
 <html>
 <head>
 <title>CHECK LOGIN PAGE</title>
+    <?php
+    $username = $_POST['user'];
+    $password = $_POST['password'];
+
+    if($username == "Mike" && $password == '123456') {
+        setcookie('user', $username);
+        setcookie('access_level_cookie', 'standarduser');
+    }
+    header('location: loggedin.php');
+    ?>
+
 </head>
 <header>
 
